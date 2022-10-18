95f531711fecc8df9739b9f2fd7ffe621a23d0f4
acShake7 <acShake7@protonmail.com>
2022-10-18 12:51:43 +0100

    2nd session of A softer side project - got it to extract the tag and the image source. Got them in a list form and now need to clean up functions and set up some proper sanity checks.

--- 90_Projects/A_Softer_Side/softer_side.py
+++ 90_Projects/A_Softer_Side/softer_side.py
@@ -3,6 +3,33 @@ A Softer Side
 Ver: 0.1
 '''
 
+'''
+****************************************
+TO DO
+1. the function defitions - make them into separate packages so that this is cleaner to read.
+
+2. Once the get_html() returns make sure its not a None object. 
+If it is, what then?
+    - in the case of the Archive page, that is easy
+    - in the case of one of the 1478 pages being returned there is a possiblity that one of
+     those links might be corrupted or a trap to trip up the scraper. have to tackle this to be flagged
+     and for the scraper to resume to the next link.
+
+3. Define functions to access the tags (on Archive page and on each of the 1478 links) - If you can reuse
+    function for the 2 cases that will be golden.
+    Now, when you access those tags, you need a check for Attribute error (you dont right now)
+    And then you need to see how you tackle the attribute error in both cases.
+
+4. Working in a list is mental. Need to convert it to a file on your drive so that you dont have to
+    keep querying the server for the same info.
+
+5. Saving the images - remember to append the index number to the filename so you have an order to it.
+
+6. Database solution to the lists and the images.
+
+****************************************
+'''
+
 # Import Packages
 from urllib.request import urlopen
 from urllib.error import HTTPError, URLError
@@ -40,7 +67,9 @@ def get_html(link):
 # Get HTML
 html = get_html(link)
 
+
 '''
+NEED TO CHECK THAT THE HTML BEING RETURNED IS NOT None OBJECT (i.e one of the errors caught)
 ACCESSING THE BEAUTIFUL SOUP OBJECT AND THE TAG SHOULD THEN GET PUSHED
 TO A FUNCTION AND GET CHECKED FOR ATTRIBUTE ERROR 
 Perhaps one for grabbing the A tags from the links
@@ -52,17 +81,50 @@ Do a killer way to the club these 2 funcions if you can!
 
 bs = BeautifulSoup(html, 'html.parser')
 
-# Searching by style - this works
-# nameList = bs.table.find_all('a', {'style':'text-decoration:underline'})
+# Approach 1: Searching by style - this works
+# pageLinkList = bs.table.find_all('a', {'style':'text-decoration:underline'})
 
-# Searching by regex of the href
+# Approach 2: Searching by regex of the href
 # in the link 'php?id' was the problem cause the ? was being interreted as an optional. Had to use the escape sequence '\?'
-nameList = bs.table.find_all('a', {'href': re.compile(r'http://www.asofterworld.com/index.php\?id=\d+')})
+tagList = bs.table.find_all('a', {'href': re.compile(r'http://www.asofterworld.com/index.php\?id=\d+')})
 
 # Check to see how many image links were found and physically check 
 # on the webpage that number is the same - should be 1248
-print(len(nameList)) # should be 1248
+print("\nNumber of links found = " + str(len(tagList))) # should be 1248
+print("Sanity Check - Number of Links - should be 1248.\n\n")
 
 
-for navlink in nameList[:5]:
-    print(navlink)
+# Check
+# View a subset of the <a> tags thrown up...
+# for navlink in pageLinkList[:5]:
+#     print(navlink)
+
+# Lists to populate the link to the image page, the title text, and eventually the link to the image.
+pageList = []
+titleTextList = []
+imageSrc = []
+
+# populating the lists with the link and the title text
+# PS!!!! IMPORTANT - I AM ONLY SCRAPING 5 IMAGES. ONCE THE CODE IS SET I WILL SCRAPE THE REST. CHANGE THE RANGE BELOW WHEN YOU ARE READY TO DO SO!
+for link in tagList[0:5]:
+    titleTextList.append(link.get_text())
+    pageList.append(link.attrs['href'])
+
+    # Following the link to get the HTML
+    pageLinkHTML = get_html(link=link.attrs['href'])
+    bs = BeautifulSoup(pageLinkHTML, 'html.parser')
+    # Access the tag that contains the image
+    imgTag = bs.find('div', id='comicimg').img
+    # SANITY : Title chech
+    print(link.get_text() == imgTag.attrs['title'])
+    
+    # Add the image src to the list
+    imageSrc.append(imgTag.attrs['src'])
+
+
+
+# Sanity Check (checking 5) - works
+# for i in range(0,5):
+#     print(pageList[i])
+#     print(titleTextList[i])
+#     print(imageSrc[i])
